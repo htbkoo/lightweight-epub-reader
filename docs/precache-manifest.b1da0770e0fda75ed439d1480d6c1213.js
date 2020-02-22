0b60c4334bf684e88452691ecaaed3c702dd9620
htbkoo <h1017760@connect.hku.hk>
2020-02-22 15:01:31 +0800

    rebuilt `/docs/`

---
+++ docs/precache-manifest.b1da0770e0fda75ed439d1480d6c1213.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30724c083fc3dd19c38c599a0618a708",
+    "url": "/lightweight-epub-reader/index.html"
+  },
+  {
+    "revision": "f8458fec81b81545e52d",
+    "url": "/lightweight-epub-reader/static/css/main.986bf13c.chunk.css"
+  },
+  {
+    "revision": "456766cc8ef88b1853f6",
+    "url": "/lightweight-epub-reader/static/js/2.d7e0f64e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/lightweight-epub-reader/static/js/2.d7e0f64e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8458fec81b81545e52d",
+    "url": "/lightweight-epub-reader/static/js/main.bda5b2c0.chunk.js"
+  },
+  {
+    "revision": "bec55e5b7b7ccfe2193e",
+    "url": "/lightweight-epub-reader/static/js/runtime-main.4fbdd8a8.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/lightweight-epub-reader/static/media/logo.ee7cd8ed.svg"
+  }
+]);
